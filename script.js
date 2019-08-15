f1be19e5072b01b0e3a2136bebdbad2cbf92b445
mradrian <mr.podkalicki.adrian94@gmail.com>
2019-08-15 17:21:14 +0200

    new addTask function,one list class

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 class List{
-    constructor(name=`List created ${date}`,date){
+    constructor(name,date,){
         this.name=name;
         this.date=date;
+        this.container=[];
     };
-    show(){
-        return console.log(`${this.name}:${this.date}`)
-    };
+   
+    addTask(content){
+        this.content=content;
+        console.log(this.container.length,"lenght")
+        this.number = this.container.length+1;
+        const task=`${this.number}.${this.content}:${this.date}`;
+        this.container.push(task);
+        return this.container
     
+    }
+    deleteTask(index){
+        this.inex=index;
+        
+    }
+    show(){
+        console.log(`${this.name}:${this.date}`)
+
+        console.log(this.container,"container")
+    }
 }
 
-class Task{
+class Task extends List{
     constructor(number,name,date){
-        List.call(this,name,date)
+        super(name,date);
         this.number=number;
     }
 }
@@ -20,27 +36,25 @@ const main=()=>{
     var date = new Date();
     window.alert("Hi, it's to do apps. Press ok to continue.");
     let run=true;
-    const firstListName=prompt("Please give name for List")
+    let firstListName=prompt("Please give name for List")
+    firstListName = firstListName == "" ? "List created:" : firstListName;
     const day = date.getDate();
     const month = date.getMonth();
     const year = date.getFullYear();
     let ListCreatedDate = `${day}-${month}-${year}`;
-    const listOne = new List(firstListName, ListCreatedDate);
-    console.log(listOne.show());
+    const listOne = new List(firstListName,ListCreatedDate);
+    listOne.show();
 
     while(run==true){
-        let count=1;
         const action = parseInt(prompt("Please choose action.1-add task  2-delete task 0-exit"),10);
         switch(action){
             case 1: 
-                const day=date.getDate();
-                const month = date.getMonth();
-                const year=date.getFullYear();
-                let ListCreatedDate=`${day}-${month}-${year}`
+           
                 const contentTask = prompt("Give a task");
-                const task = new Task(count, contentTask, ListCreatedDate);
-                console.log(task.show());
-                    
+                listOne.addTask(contentTask);
+                listOne.show();
+
+               
                 break;
             case 0:
                 run=false;
@@ -48,6 +62,7 @@ const main=()=>{
 
                 
         }
+        
     }
 
 }
